4ee89bc494c36826b751febca900dcf43b5c9c7a
Jonathan <>
2020-09-23 09:54:08 +0200

    Basic Kademlia & Bootstrap

--- main.go
+++ main.go
@@ -2,41 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"net"
 	"strconv"
 )
 
-func main() {
-	network := NewNetwork(nil)
+func bootstrap(network *Network, nodeIPs []string) {
+	successfulPing := false
+	for _, stringIP := range nodeIPs {
+		ip := net.ParseIP(stringIP)
+		if ip != nil {
+			fmt.Println("Pinging bootstrap node " + ip.String())
+			contact := NewContact(nil, ip)
+			success := network.SendPingMessage(&contact)
 
-	fmt.Println("Node initalized on port 20000!");
-
-	go func() {
-		buffer := make([]byte, 256)
-		for {
-			length, address, _ := network.connection.ReadFromUDP(buffer)
-			fmt.Println("Received \"" + string(buffer[0:length]) + "\" from " + address.IP.String() + ":" + strconv.Itoa(address.Port))
+			if success {
+				fmt.Println("Ping successful!");
+				successfulPing = true
+			} else {
+				fmt.Println("Ping failed!");
+			}
 		}
-	}()
-
-	for {
-		var input string
-
-		fmt.Println("Enter IP to ping: ")
-		fmt.Scanln(&input)
-		ip := net.ParseIP(input)
-
-		fmt.Println("Enter Port to ping: ")
-		fmt.Scanln(&input)
-		port, _ := strconv.Atoi(input)
-
-		fmt.Println("Enter message: ")
-		fmt.Scanln(&input)
-		message := input
-		buffer := []byte(message)
-
-		network.connection.WriteToUDP(buffer, &net.UDPAddr{IP:ip, Port:port, Zone:""})
-
-		fmt.Println("Sent \"" + message + "\" to " + ip.String() + ":" + strconv.Itoa(port))
 	}
+
+	if !successfulPing {
+		fmt.Println("No bootstrap nodes answered. Bootstrap failed");
+	} else {
+		//TODO self lookup
+
+		//Successful bootstrap
+		fmt.Println("Bootstrap successful!");
+	}
+}
+
+func main() {
+	kademlia := NewKademlia(NewRandomKademliaID())
+	network := NewNetwork(kademlia)
+
+	fmt.Println("Node initalized on port " + strconv.Itoa(standardPort) + "!");
+
+	bootstrap(network, os.Args)
+
+	//Sleep forever, work done in goroutines
+	select{}
 }
